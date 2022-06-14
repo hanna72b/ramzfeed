e80238246049b590f40354bc354c85c6f3a1a7f4
mohsen <rahimpour1985@gmail.com>
2022-06-15 00:29:04 +0430

    redesign tweet page

--- pages/tweets/index.vue
+++ pages/tweets/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="mx-5 tweets-page">
-    <div class="d-flex justify-space-between">
-      <h3 class="mt-3 mb-1 text_color_grey--text">آخرین توییت ها</h3>
-    </div>
+    <v-row justify="center">
+      <v-col cols="12" md="8" lg="5" xl="4">
+        <div class="d-flex justify-space-between">
+          <h3 class="mt-3 mb-1 text_color_grey--text">آخرین توییت ها</h3>
+        </div>
 
-    <h6 class="orange_color_1--text mb-3 font-weight-light">
-      در این بخش می‌توانید به صوررت لحظه‌ای ،توییت‌های تریدرهای حرفه ای را به
-      زبان فارسی مشاهده کنید
-    </h6>
-    <v-row>
-      <v-col cols="12" md="6">
-        <div :class="[tweet_list, 'news-title']">
+        <h5 class="orange_color_1--text mb-3 font-weight-light">
+          در این بخش می‌توانید به صوررت لحظه‌ای ،توییت‌های تریدرهای حرفه ای را
+          به زبان فارسی مشاهده کنید
+        </h5>
+        <div class="news-title">
           <div v-if="tweets.length === 0">
             <loading></loading>
           </div>
@@ -20,12 +20,9 @@
               :key="index"
               class="pt-4 font-font-weight-light"
             >
-              <div class="d-flex">
-                <a
-                  @click="gotoTweet(tweet.slug)"
-                  :class="[active[tweet.slug], 'text_color_grey--text']"
-                >
-                  <v-avatar size="30">
+              <div class="profile d-flex">
+                <span>
+                  <v-avatar size="70">
                     <v-img
                       :src="
                         getPath('influencer') + tweet.influencer.profile_image
@@ -34,158 +31,65 @@
                       class="mx-auto d-block"
                     ></v-img>
                   </v-avatar>
-                  <span
-                    class="font-weight-regular my-2"
-                    v-html="splitTweet(tweet.text_translated)"
+                </span>
+                <span class="mr-3 mt-1 tex-right">
+                  <h2 class="cyan--text n">
+                    {{ tweet.influencer.name }}
+                  </h2>
+                  <h4 class="text_color_grey--text sn">
+                    @{{ tweet.influencer.screen_name }}
+                  </h4>
+
+                  <h4 class="text_color_grey--text f">
+                    {{ tweet.influencer.followers }} followers
+                  </h4>
+                </span>
+              </div>
+
+              <div class="">
+                <h3
+                  class="tweet mt-8"
+                  dir="rtl"
+                  v-html="tweet.text_translated"
+                ></h3>
+
+                <div v-if="tweet.attachment" class="media mt-3">
+                  <div
+                    v-for="(attachment, index) in tweet.attachment"
+                    :key="index"
                   >
-                  </span>
-                </a>
+                    <div
+                      v-if="attachment.type === 'url'"
+                      class="mt-2 tweet-link"
+                    >
+                      <v-icon color="cyan" small> mdi-open-in-new </v-icon>
+                      <a :href="attachment.url" target="_blank">{{
+                        attachment.url
+                      }}</a>
+                    </div>
+                    <div v-if="attachment.type === 'img'" class="mt-2">
+                      <v-img
+                        :src="getPath('twitter') + attachment.path"
+                        @click="dialog_img = true"
+                        class="cursor-pointer mx-auto"
+                        :aspect-ratio="16 / 9"
+                        width="100%"
+                      ></v-img>
+                    </div>
+                  </div>
+                </div>
+                {{
+                  toJalali(new Date(tweet.published_at).getTime() / 1000, true)
+                }}
               </div>
-              <div class="d-flex justify-end">
-                <v-btn
-                  color="cyan"
-                  icon
-                  small
-                  @click="gotoTweet(tweet.slug)"
-                  class="ml-5"
-                >
-                  <v-icon right small>mdi-arrow-left</v-icon>
-                </v-btn>
-              </div>
+
               <v-divider class="mt-3"></v-divider>
             </div>
           </div>
         </div>
-        <!-- ====================================================================== -->
-        <!-- ==================Display tweet detail only in mobile==================== -->
-        <!-- ====================================================================== -->
-        <div :class="[tweet_detail, 'details-of-tweet']">
-          <div v-if="!tweet" class="news-title">
-            <v-avatar size="80">
-              <img src="~/assets/images/logo.png" width="80" />
-            </v-avatar>
-          </div>
-          <v-card v-else dark color="#3f4248">
-            <div class="d-flex justify-end">
-              <div class="py-5">
-                <v-btn small @click="goToMainTweets">
-                  <span>بازگشـت</span>
-                  <v-icon right>mdi-arrow-left</v-icon>
-                </v-btn>
-              </div>
-            </div>
-            <v-card-text class="px-10 py-6">
-              <div v-if="tweet.influencer" class="profile d-flex">
-                <span>
-                  <v-avatar size="80">
-                    <v-img
-                      :src="
-                        getPath('influencer') + tweet.influencer.profile_image
-                      "
-                      width="100%"
-                      class="mx-auto d-block"
-                    ></v-img>
-                  </v-avatar>
-                </span>
-                <span class="mr-3 mt-5 tex-right">
-                  <h2 class="cyan--text n">
-                    {{ tweet.influencer.name }}
-                  </h2>
-                  <!-- <h4 class="text_color_grey--text mt-1 sn">
-                  {{ tweet.influencer.screen_name }}@
-                </h4> -->
-
-                  <h4 class="text_color_grey--text mt-1 f">
-                    <span>followers:</span>
-                    {{ tweet.influencer.followers }}
-                  </h4>
-                </span>
-              </div>
-              <h3 class="tweet mt-8" v-html="tweet.text_translated"></h3>
-              <div v-if="tweet.attachment" class="media mt-3">
-                <div
-                  v-for="(attachment, index) in tweet.attachment"
-                  :key="index"
-                >
-                  <div v-if="attachment.type === 'url'" class="mt-2 tweet-link">
-                    <v-icon color="cyan" small> mdi-open-in-new </v-icon>
-                    <a :href="attachment.url" target="_blank">{{
-                      attachment.url
-                    }}</a>
-                  </div>
-                  <div v-if="attachment.type === 'img'" class="mt-2">
-                    <v-img
-                      :src="getPath('twitter') + attachment.path"
-                      @click="dialog_img = true"
-                      class="cursor-pointer mx-auto"
-                      :aspect-ratio="16 / 9"
-                      width="100%"
-                    ></v-img>
-                  </div>
-                </div>
-              </div>
-            </v-card-text>
-          </v-card>
-        </div>
       </v-col>
 
-      <v-col cols="12" md="6" class="d-none d-md-flex">
-        <v-card v-if="tweet" dark color="#3f4248" width="100%">
-          <v-card-text class="px-10 py-6">
-            <div class="profile d-flex">
-              <span>
-                <v-avatar size="90">
-                  <v-img
-                    :src="
-                      getPath('influencer') + tweet.influencer.profile_image
-                    "
-                    width="100%"
-                    class="mx-auto d-block"
-                  ></v-img>
-                </v-avatar>
-              </span>
-              <span class="mr-3 mt-5 tex-right">
-                <h2 class="cyan--text n">
-                  {{ tweet.influencer.name }}
-                </h2>
-                <!-- <h4 class="text_color_grey--text mt-1 sn">
-                  {{ tweet.influencer.screen_name }}@
-                </h4> -->
-
-                <h4 class="text_color_grey--text mt-1 f">
-                  <span>followers:</span>
-                  {{ tweet.influencer.followers }}
-                </h4>
-              </span>
-            </div>
-            <h3
-              class="tweet mt-8"
-              dir="rtl"
-              v-html="tweet.text_translated"
-            ></h3>
-
-            <div v-if="tweet.attachment" class="media mt-3">
-              <div v-for="(attachment, index) in tweet.attachment" :key="index">
-                <div v-if="attachment.type === 'url'" class="mt-2 tweet-link">
-                  <v-icon color="cyan" small> mdi-open-in-new </v-icon>
-                  <a :href="attachment.url" target="_blank">{{
-                    attachment.url
-                  }}</a>
-                </div>
-                <div v-if="attachment.type === 'img'" class="mt-2">
-                  <v-img
-                    :src="getPath('twitter') + attachment.path"
-                    @click="dialog_img = true"
-                    class="cursor-pointer mx-auto"
-                    :aspect-ratio="16 / 9"
-                    width="100%"
-                  ></v-img>
-                </div>
-              </div>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
+      <!-- ==============================================DETAILS======================================= -->
     </v-row>
 
     <v-dialog
--- plugins/dynamicDomain.js
+++ plugins/dynamicDomain.js
@@ -2,7 +2,7 @@ export default ({ app, req }, inject) => {
   const host = process.server ? req.headers.host : window.location.host
   let baseUrl = ''
   if (host === 'localhost:3000') {
-    baseUrl = 'http://localhost:8000'
+    baseUrl = 'https://api.ramzfeed.com'
   } else {
     baseUrl = 'https://api.ramzfeed.com'
   }
