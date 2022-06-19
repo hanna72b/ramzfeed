4c2ab608858b21fc505f145a95b347eb6dc5cf8d
Shapour Rahimpour <rahimpour1985@gmail.com>
2022-06-19 17:18:48 +0430

    google tag module

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,8 +23,28 @@ export default {
     middleware: 'routing',
   },
 
-  googleAnalytics: {
-    id: 'G-LSXZXFNTGN',
+  'google-gtag': {
+    id: 'G-LSXZXFNTGN', // required
+    config: {
+      // this are the config options for `gtag
+      // check out official docs: https://developers.google.com/analytics/devguides/collection/gtagjs/
+      anonymize_ip: true, // anonymize IP
+      send_page_view: false, // might be necessary to avoid duplicated page track on page reload
+      linker: {
+        domains: ['ramzfeed.com'],
+      },
+    },
+    debug: true, // enable to track in dev mode
+    disableAutoPageTrack: false, // disable if you don't want to track each page route with router.afterEach(...)
+    // optional you can add more configuration like [AdWords](https://developers.google.com/adwords-remarketing-tag/#configuring_the_global_site_tag_for_multiple_accounts)
+    // additionalAccounts: [
+    //   {
+    //     id: 'AW-XXXX-XX', // required if you are adding additional IDs
+    //     config: {
+    //       send_page_view: false, // optional configurations
+    //     },
+    //   },
+    // ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -82,7 +102,7 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
-    '@nuxtjs/google-analytics',
+    '@nuxtjs/google-gtag',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
